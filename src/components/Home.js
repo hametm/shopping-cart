a1dbe19eade354b3091c025506fce7bfc6ad7921
Morgan Hamet <morganhamet@gmail.com>
2022-07-03 21:52:49 -0700

    Add home and shop pages with nav

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import Nav from "./Nav";
+import "../styles/style.css";
+
+
+function Home() {
+  return (
+    <div className="Home">
+      <Nav />
+      Hello from Home!
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Shop.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav";
+
+function Shop() {
+    return (
+        <div>
+            <Nav />
+            Hello from Shop!
+        </div>
+    );
+}
+
+export default Shop;
