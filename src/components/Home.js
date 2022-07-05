4cd38ce1126049eabf0171fd2d32864b532d3cc8
Morgan Hamet <morganhamet@gmail.com>
2022-07-04 23:49:25 -0700

    Add UI to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,20 @@
 import Nav from "./Nav";
 import "../styles/style.css";
+import { Link } from "react-router-dom";
 
 
 function Home() {
   return (
-    <div className="Home">
+    <div className="container">
       <Nav />
-      <h1>Hello from Home!</h1>
+      <main id="homeContainer">
+        <div id="homeText">
+          <h1>fresh fruit, right when you need it.</h1>
+          <Link style={{textDecoration: "none"}} to="/shop">
+            <h2>shop all fruit</h2>
+          </Link>
+        </div>
+      </main>
     </div>
   );
 }
